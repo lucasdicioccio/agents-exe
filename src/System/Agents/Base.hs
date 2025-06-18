17db23de97f9bfdfb2076eadefcd54653bfeea6c
Lucas DiCioccio <lucas@dicioccio.fr>
2025-06-18 11:35:21 +0200

    move agent-description to base

--- src/System/Agents/Base.hs
+++ src/System/Agents/Base.hs
@@ -1,12 +1,15 @@
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE OverloadedRecordDot #-}
 
 module System.Agents.Base where
 
 import Data.Aeson (FromJSON, ToJSON)
+import qualified Data.Aeson as Aeson
 import Data.Text (Text)
 import Data.UUID (UUID)
 import qualified Data.UUID.V4 as UUID
+import GHC.Generics (Generic)
 
 type AgentSlug = Text
 type AgentAnnounce = Text
@@ -24,3 +27,26 @@ newtype ConversationId = ConversationId UUID
 newConversationId :: IO ConversationId
 newConversationId =
     ConversationId <$> UUID.nextRandom
+
+-------------------------------------------------------------------------------
+data OpenAIAgent
+    = OpenAIAgent
+    { slug :: Text
+    , apiKeyId :: Text
+    , flavor :: Text
+    , modelUrl :: Text
+    , modelName :: Text
+    , announce :: Text
+    , systemPrompt :: [Text]
+    , toolDirectory :: FilePath
+    }
+    deriving (Show, Ord, Eq, Generic)
+instance ToJSON OpenAIAgent
+instance FromJSON OpenAIAgent
+
+data AgentDescription
+    = OpenAIAgentDescription OpenAIAgent
+    | Unspecified Aeson.Value
+    deriving (Show, Ord, Eq, Generic)
+instance ToJSON AgentDescription
+instance FromJSON AgentDescription
