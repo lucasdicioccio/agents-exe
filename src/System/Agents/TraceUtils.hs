a28c6d5212401b8b039344f543c301cd61fe48c8
Lucas DiCioccio <lucas@dicioccio.fr>
2025-07-05 11:43:45 +0200

    Some cleanup en-passant.

---
+++ src/System/Agents/TraceUtils.hs
@@ -0,0 +1,18 @@
+module System.Agents.TraceUtils where
+
+import Prod.Tracer (Tracer (..))
+
+import System.Agents.Agent (Trace (..))
+import qualified System.Agents.LLMs.OpenAI as OpenAI
+import qualified System.Agents.Runtime as Runtime
+
+-------------------------------------------------------------------------------
+traceWaitingOpenAIRateLimits ::
+    OpenAI.ApiLimits ->
+    (OpenAI.WaitAction -> IO ()) ->
+    Tracer IO Trace
+traceWaitingOpenAIRateLimits lims onWait = Tracer f
+  where
+    f (AgentTrace (Runtime.AgentTrace_Conversation _ _ _ (Runtime.LLMTrace _ tr))) =
+        runTracer (OpenAI.waitRateLimit lims onWait) tr
+    f _ = pure ()
