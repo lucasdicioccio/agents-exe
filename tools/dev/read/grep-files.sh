04bed1b37642bc9fe3dc5a41176865b08e98af69
Lucas DiCioccio <lucas@dicioccio.fr>
2026-08-24 04:25:45 +0200

    Hide more files from grep outputs.

--- tools/dev/read/grep-files.sh
+++ tools/dev/read/grep-files.sh
@@ -27,7 +27,7 @@ if [[ "$1" == "run" ]]; then
     fi
     echo "---"
     pattern="$2"
-    git grep -n -I "${pattern}" -- . ":!tasks-sessions/*"
+    git grep -n -I "${pattern}" -- . ":!manual-sessions/*" ":!tasks-sessions/*"
     exit 0
 fi
 
